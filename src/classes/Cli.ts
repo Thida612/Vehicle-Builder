aa226b0a35573597357da22f27280d0f06472664
Thida612 <angelic722@gmail.com>
2024-10-25 21:35:04 -0500

    Implement towing feature for trucks

--- src/classes/Cli.ts
+++ src/classes/Cli.ts
@@ -411,6 +411,19 @@ class Cli {
           }
         }
         // TODO: add statements to perform the tow action only if the selected vehicle is a truck. Call the findVehicleToTow method to find a vehicle to tow and pass the selected truck as an argument. After calling the findVehicleToTow method, you will need to return to avoid instantly calling the performActions method again since findVehicleToTow is asynchronous.
+        else if (answers.action === 'TOW a vehicle') {
+          for (let i = 0; i < this.vehicles.length; i++) {
+            if (this.vehicles[i].vin === this.selectedVehicleVin) {
+            if (this.vehicles[i] instanceof Truck)
+               {
+                this.findVehicleToTow(this.vehicles[i]as Truck);return;
+               } else { console.log('This vehicle cannot tow another vehicle');
+  
+            }
+          }
+      
+        }
+        
         // TODO: add statements to perform the wheelie action only if the selected vehicle is a motorbike
         else if (answers.action === 'Select or create another vehicle') {
           // start the cli to return to the initial prompt if the user wants to select or create another vehicle
