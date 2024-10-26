fe66c6ffde2bc963069db8e13645adf2547940d6
Thida612 <angelic722@gmail.com>
2024-10-25 22:45:44 -0500

    Added constructor and call the constructor of the parent class vehicle, the constructor initialize the properties of the truck class

--- src/classes/Truck.ts
+++ src/classes/Truck.ts
@@ -19,14 +19,35 @@ class Truck extends Vehicle implements AbleToTow{
   wheels: Wheel[];
   towingCapacity: number;
   
-  // TODO: The properties should include vin, color, make, model, year, weight, top speed, wheels, and towing capacity
-  
+   // TODO: The properties should include vin, color, make, model, year, weight, top speed, wheels, and towing capacity
   // TODO: The types should be as follows: vin (string), color (string), make (string), model (string), year (number), weight (number), topSpeed (number), wheels (Wheel[]), towingCapacity (number)
+ // TODO: Create a constructor that accepts the properties of the Truck class
+ constructor(
+  vin: string,
+  color: string,
+  make: string,
+  model: string,
+  year: number,
+  weight: number,
+  topSpeed: number,
+  wheels: Wheel[],
+  towingCapacity: number,
+) {  
 
-  // TODO: Create a constructor that accepts the properties of the Truck class
-    // TODO: The constructor should call the constructor of the parent class, Vehicle
-    // TODO: The constructor should initialize the properties of the Truck class
-    // TODO: The constructor should check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
+
+ // TODO: The constructor should call the constructor of the parent class, Vehicle
+ super();  
+ // TODO: The constructor should initialize the properties of the Truck class
+ this.vin = vin;
+ this.color = color;
+ this.make = make;
+ this.model = model;
+ this.year = year;
+ this.weight = weight;
+ this.topSpeed = topSpeed;
+ this.wheels = wheels;
+ this.towingCapacity = towingCapacity;  
+ // TODO: The constructor should check if the wheels array has 4 elements and create 4 new default Wheel objects if it does not
 
   // TODO: Implement the tow method from the AbleToTow interface
   tow(vehicle: Truck | Motorbike | Car): void {
